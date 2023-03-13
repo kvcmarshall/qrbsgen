576d40f2a7163ee5f6480b85699d634afc2719db
Kate-Marshall <kate.marshall@ibm.com>
2023-03-13 21:08:19 +0000

    update setup with new PyPI version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
     name='qrbsgen',
     packages=['qrbsgen'],  # this must be the same as the name above
-    version='0.0.0',
+    version='0.0.1',
     description='A simple Python library for encoding Quantum Rule Based Systems',
     author='Kate Marshall',
     author_email='kate.marshall@ibm.com',
